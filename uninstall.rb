79526bdd3b3fc47bd2614866a8aaedcb8d2fdaaf
robmckinnon <rob@theyworkforyou.co.nz>
2009-09-28 00:38:28 +0100

    sudo rm migration file

--- uninstall.rb
+++ uninstall.rb
@@ -4,5 +4,5 @@ if migration_file && migration_file[/[^\d](\d+)_create_sic_uk_tables.rb/]
   version = $1
   puts `cd #{RAILS_ROOT}; rake db:migrate:down VERSION=#{version}`
   puts "removing: #{migration_file}"
-  puts `rm #{migration_file}`
+  puts `sudo rm #{migration_file}`
 end
