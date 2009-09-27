4c1cd4643ef39a2946aa119d4bd026613dde7f26
robmckinnon <rob@theyworkforyou.co.nz>
2009-09-28 00:20:51 +0100

    more verbose

--- install.rb
+++ install.rb
@@ -9,11 +9,14 @@ end
 
 migration_file = `ls #{RAILS_ROOT}/db/migrate/*create_sic_uk_tables.rb`
 
-puts "trying to run: #{migration_file}"
 
-if File.exists?(migration_file)
+if File.exist?(migration_file)
   migration = IO.read("#{RAILS_ROOT}/vendor/plugins/sic-uk/data/create_sic_uk_tables.rb")
+  puts "creating migration: #{migration}"
   File.open(migration_file, 'w') {|file| file.write(migration)}
 
+  puts "trying to run: #{migration_file}"
   puts `cd #{RAILS_ROOT}; rake db:migrate`
+else
+  puts "cannot find: #{migration_file}"
 end
