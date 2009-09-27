90ef78060bbe177c8a6d28d24fa3a1d31b543418
robmckinnon <rob@theyworkforyou.co.nz>
2009-09-28 00:32:25 +0100

    cleaned up install.rb

--- install.rb
+++ install.rb
@@ -1,6 +1,3 @@
-# Install hook code here
-# path = File.join(File.dirname(__FILE__), '..', '..', '..')
-
 begin
   puts `cd #{RAILS_ROOT}; ./script/generate migration create_sic_uk_tables`
 rescue Exception => e
@@ -63,14 +60,8 @@ class CreateSicUkTables < ActiveRecord::Migration
 end
 %
 
+puts "writing migration"
+File.open(migration_file, 'w') {|file| file.write(migration)}
 
-# if File.exist?(migration_file)
-  # migration = IO.read("#{RAILS_ROOT}/vendor/plugins/sic-uk/data/create_sic_uk_tables.rb")
-  puts "creating migration: #{migration}"
-  File.open(migration_file, 'w') {|file| file.write(migration)}
-
-  puts "trying to run: #{migration_file}"
-  puts `cd #{RAILS_ROOT}; rake db:migrate`
-# else
-  # puts "cannot find: #{migration_file}"
-# end
+puts "running: #{migration_file}"
+puts `cd #{RAILS_ROOT}; rake db:migrate`
